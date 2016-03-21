1411d3b652d2ee9c6dc673a691eb4e06943f32a1
Kate Sowles <katesowles@gmail.com>
2016-03-21 13:55:41 -0700

    adding problem three + gulp files

--- test.js
+++ test.js
@@ -2,14 +2,22 @@
 //write tests first in test.js to make sure they fail
 //APC the test.js file to prove we wrote them first!
 //complete the first six questions in test.js
-//write your coe as functions in app.js
+//write your coe as functions in compute.js
+
+var assert = require('assert');
+var compute = require('compute.js');
 
 //problem one
-  it ('test that 10 + 5 = 15', function () { assert(15 == app.sum(10,5)); });
-  it ('test that 8 + 16 = 24', function () { assert(24) == app.sum(8,16); });
-  it ('test that 33 + 21 = 54', function () { assert(54) == app.sum(33,21); });
+  it ('test that 10 + 5 = 15', function () { assert(15 == compute.sum(10,5)); });
+  it ('test that 8 + 16 = 24', function () { assert(24 == compute.sum(8,16)); });
+  it ('test that 33 + 21 = 54', function () { assert(54 == compute.sum(33,21)); });
 
 //problem two
-  it ('test that 3 * 5 = 15'), function () { assert(15) == app.multiply(3,5)); });
-  it ('test that 2 * 4 = 8'), function () { assert(8) == app.multiply(2,4)); });
-  it ('test that 3 * 17 = 51'), function () { assert(51) == app.multiply(3,17)); });
+  it ('test that 3 * 5 = 15'), function () { assert(15 == compute.multiply(3,5)); });
+  it ('test that 2 * 4 = 8'), function () { assert(8 == compute.multiply(2,4)); });
+  it ('test that 3 * 17 = 51'), function () { assert(51 == compute.multiply(3,17)); });
+
+//problem three
+  it ('test that it shows sum AND multiplication of 1, 3, 7'), function () { assert( == compute.sumAndMultiply(1,3,7)); });
+  it ('test that it shows sum AND multiplication of 5, 8, 13'), function () { assert( == compute.sumAndMultiply(5,8,13)); });
+  it ('test that it shows sum AND multiplication of 14, 15, 3'), function () { assert( == compute.sumAndMultiply(14,15,3)); });
