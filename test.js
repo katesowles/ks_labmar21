fe00e1d87ed94ada5bd4762b5e6c97c04b01d166
Kate Sowles <katesowles@gmail.com>
2016-03-21 13:38:28 -0700

    pushing problem one

---
+++ test.js
@@ -0,0 +1,10 @@
+//create new branch for each problem
+//write tests first in test.js to make sure they fail
+//APC the test.js file to prove we wrote them first!
+//complete the first six questions in test.js
+//write your coe as functions in app.js
+
+//problem one
+  it ('should test that 10 + 5 = 15', function () { assert(15 == app.sum(10,5)); });
+  it ('should test that 8 + 16 = 24', function () { assert(24) == app.sum(8,16); });
+  it ('should test that 33 + 21 = 54', function () { assert(54) == app.sum(33,21); });
