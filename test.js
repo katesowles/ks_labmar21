2475ddf1001e31b1dbc93be1c0a681b002c36bcd
Kate Sowles <katesowles@gmail.com>
2016-03-21 13:42:48 -0700

    pushing prbblem two

--- test.js
+++ test.js
@@ -5,6 +5,11 @@
 //write your coe as functions in app.js
 
 //problem one
-  it ('should test that 10 + 5 = 15', function () { assert(15 == app.sum(10,5)); });
-  it ('should test that 8 + 16 = 24', function () { assert(24) == app.sum(8,16); });
-  it ('should test that 33 + 21 = 54', function () { assert(54) == app.sum(33,21); });
+  it ('test that 10 + 5 = 15', function () { assert(15 == app.sum(10,5)); });
+  it ('test that 8 + 16 = 24', function () { assert(24) == app.sum(8,16); });
+  it ('test that 33 + 21 = 54', function () { assert(54) == app.sum(33,21); });
+
+//problem two
+  it ('test that 3 * 5 = 15'), function () { assert(15) == app.multiply(3,5)); });
+  it ('test that 2 * 4 = 8'), function () { assert(8) == app.multiply(2,4)); });
+  it ('test that 3 * 17 = 51'), function () { assert(51) == app.multiply(3,17)); });
